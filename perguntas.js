019b7409554fba0d521707a8a6e0c403fbf6aa94
luludapomerania07 <168845414+luludapomerania07@users.noreply.github.com>
2024-12-02 08:20:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Entretenimento',
+    'Quais são os personagens principais de The O.C??',
+    'Ryan, Seth, Marissa, Summer, Luke, Sandy, Kirsten, Jules, Jimmy'
+)
+
+criaCartao(
+    'Entretenimento',
+    'Em que cidade se passa a série Gossip Girl?',
+    'Nova York, NY, EUA'
+)
+
+criaCartao(
+    'Entretenimento',
+    'Quais são os personagens principais de Gilmore Girls??',
+    'Rory Gilmore, Lorelai Gilmore, Luke Danes, Sookie, Emily Gilmore'
+)
+
+criaCartao(
+    'Entretenimento',
+    'Qual o nome da penitenciária onde Prison Break é ambientada??',
+    'Fox River'
+)
